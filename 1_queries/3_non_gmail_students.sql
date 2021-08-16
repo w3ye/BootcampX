76c58b27c2afbc1550308205c39401e707722b60
William Ye <w3ye@pm.me>
2021-08-16 19:34:48 -0400

    added 1_queries

---
+++ 1_queries/3_non_gmail_students.sql
@@ -0,0 +1,6 @@
+SELECT name, id, email, cohort_id
+FROM students
+WHERE
+  email NOT LIKE '%@gmail.com'
+  AND
+  phone IS NULL;
---
+++ 1_queries/5_github_activity.sql
@@ -0,0 +1,6 @@
+SELECT name, email, phone
+FROM students
+WHERE
+  github IS NULL
+  AND
+  end_date IS NOT NULL;
