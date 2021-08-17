665af47ea9fa18e4a98d2c6447850815fcf5bc7c
William Ye <w3ye@pm.me>
2021-08-17 14:05:12 -0400

    created teachers and teachers_assistance_request table

---
+++ migrations/teachers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE teachers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE,
+  start_date DATE,
+  end_date DATE
+);
---
+++ migrations/teachers_assistance_request.sql
@@ -0,0 +1,11 @@
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
